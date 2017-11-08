4ff7820974d21bb011ceae8e6b607801d96f2658
unknown <>
2017-11-08 18:42:44 +0800

    Signed-off-by: unknown <jackxiongyahui>

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class GameManager : MonoBehaviour {
+
+
+    //private Transform startPoint;
+
+    private Transform spawnPoint;
+    private Pin currentPin;
+
+    public GameObject PinPrefab;
+    private int score = 0;
+
+    public Text scoreText;
+
+    private bool isGameOver = false;
+
+    private Camera mainCamera;
+
+    public float speed = 3f; //动画的速度
+
+	// Use this for initialization
+	void Start () {
+        //startPoint = GameObject.Find("StartPoint").transform;
+        spawnPoint = GameObject.Find("SpawnPoint").transform;
+        mainCamera = Camera.main;
+        SpawnPin();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (isGameOver)
+            return;
+
+		if(Input.GetMouseButtonDown(0))
+        {
+            score++;
+            scoreText.text = score.ToString();
+            currentPin.StartFly();  //针开始飞行
+            SpawnPin();   //重新实例化一个新的针
+        }
+	}
+
+    void SpawnPin()
+    {
+        currentPin=  GameObject.Instantiate(PinPrefab, spawnPoint.position, PinPrefab.transform.rotation).GetComponent<Pin>();  //实例化针
+    }
+
+    public void GameOver()
+    {
+        if (isGameOver)
+            return;
+        GameObject.Find("Circle").GetComponent<RotateSelf>().enabled = false;  //让球不再旋转，就是停止运行
+        StartCoroutine(GameOverAnimation());
+        isGameOver = true;
+    }
+
+    IEnumerator GameOverAnimation()
+    {
+        while (true)
+        {
+            mainCamera.backgroundColor = Color.Lerp(mainCamera.backgroundColor, Color.red, speed * Time.deltaTime);
+            mainCamera.orthographicSize = Mathf.Lerp(mainCamera.orthographicSize, 4, speed * Time.deltaTime);
+            if(Mathf.Abs(mainCamera.orthographicSize-4)<0.01f)
+            {
+                break;
+            }
+            yield return 0;
+        }
+        yield return new WaitForSeconds(0.2f);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
---
+++ Assets/Scripts/Pin.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pin : MonoBehaviour {
+
+    public float speed = 5f;   //针前进的速度
+    private bool isFly = false;     //是否进行飞行
+
+    private bool isReach = false;   //是否到达startPoint
+
+    private Vector3 targetCirclePos;  //目标位置
+
+    private Transform startPoint;
+    private Transform circle;
+
+	// Use this for initialization
+	void Start () {
+        startPoint = GameObject.Find("StartPoint").transform;
+        circle = GameObject.FindGameObjectWithTag("Circle").transform;
+        targetCirclePos = circle.position; //目标点的位置
+        targetCirclePos.y = 0.43f;    //针围绕球旋转的y的位置
+    }
+
+	// Update is called once per frame
+	void Update () {
+		if(isFly==false)
+        {
+            if(isReach==false)
+            {
+                //朝着startPoint运动
+               transform.position =  Vector3.MoveTowards(transform.position, startPoint.position, speed * Time.deltaTime); 
+               if(Vector3.Distance(transform.position,startPoint.position)<0.05f)
+                {
+                    isReach = true;  //当到达startPoint后，针就停止运动
+                }
+            }
+        }else
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetCirclePos, speed * Time.deltaTime);  //朝向圆运动
+            if(Vector3.Distance(transform.position,targetCirclePos)<0.05f)
+            {
+                transform.position = targetCirclePos;
+                transform.parent = circle;
+                isFly = false;   //当到达球后，就停止飞行
+            }
+        }
+	}
+
+    public void StartFly()
+    {
+        isFly = true;
+        isReach = true;
+    }
+
+}
